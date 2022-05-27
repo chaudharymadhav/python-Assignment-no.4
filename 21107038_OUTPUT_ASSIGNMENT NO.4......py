885496ca248ccd80442637f4f7f75ae573465022
MADHAV <104258321+chaudharymadhav@users.noreply.github.com>
2022-05-27 09:26:23 +0530

    Add files via upload

---
+++ 21107038_OUTPUT_ASSIGNMENT NO.4......py
@@ -0,0 +1,73 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\mkcsn\PycharmProjects\python College Assignement\Python Assignment No.4.py
+Enter your marks-:
+9
+your Grade is F
+Enter your marks-:
+20
+your Grade is F
+Enter your marks-:
+25
+your Grade is E
+Enter your marks-:
+35
+your Grade is E
+Enter your marks-:
+45
+your Grade is D
+Enter your marks-:
+49
+your Grade is D
+Enter your marks-:
+58
+your Grade is C
+Enter your marks-:
+64
+your Grade is B
+Enter your marks-:
+80
+your Grade is A
+Enter your marks-:
+100
+your Grade is A
+ you can enter your marks only 10 times 
+ 
+Enter the digits of any year of your choice -:1000
+No it is not a leap year
+ 
+This is a multiplication game program for kids
+question no. 1 ,  15 X 15 is
+Enter your answer:-80
+your answer is wrong!,learn tables properly
+question no. 2 ,  1 X 1 is
+Enter your answer:-1
+Right!,Answer
+question no. 3 ,  1 X 1 is
+Enter your answer:-1
+Right!,Answer
+question no. 4 ,  8 X 0 is
+Enter your answer:-0
+Right!,Answer
+question no. 5 ,  3 X 3 is
+Enter your answer:-9
+Right!,Answer
+question no. 6 ,  4 X 1 is
+Enter your answer:-4
+Right!,Answer
+question no. 7 ,  9 X 10 is
+Enter your answer:-90
+Right!,Answer
+question no. 8 ,  1 X 5 is
+Enter your answer:-5
+Right!,Answer
+question no. 9 ,  9 X 2 is
+Enter your answer:-18
+Right!,Answer
+question no. 10 ,  13 X 5 is
+Enter your answer:-65
+Right!,Answer
+There were only 10 questions ,thank you for attempting
+ 
+There are 177 candies in total
